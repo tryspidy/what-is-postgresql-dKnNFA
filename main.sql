9bd63b477ee1f0ee340c2cfa5102ca5b280129ef
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 02:54:18 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,6 @@
+PostgreSQL is a powerful, open source object-relational database system. It has more than 15 years of active development phase and a proven architecture that has earned it a strong reputation for reliability, data integrity, and correctness.
+
+This tutorial will give you a quick start with PostgreSQL and make you comfortable with PostgreSQL programming.
+
+What is PostgreSQL?
+PostgreSQL (pronounced as post-gress-Q-L) is an open source relational database management system (DBMS) developed by a worldwide team of volunteers. PostgreSQL is not controlled by any corporation or other private entity and the source code is available free of charge.
